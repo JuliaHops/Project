99bcb70896982813a1d2ff857535342ba4da05de
ivc1-5 <ulikstoluar@yandex.ru>
2022-09-27 11:52:22 +0300

    Добавила 5 задание из 1 практики

---
+++ src/lab1/task5/Arg.java
@@ -0,0 +1,9 @@
+package lab1.task5;
+
+public class Arg {
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length;i++){
+            System.out.println("Аргумент " + i + " :" + args[i]);
+        }
+    }
+}
